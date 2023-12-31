01115a9c4ef5c87c8edc26492c3144f6cfeab06c
Fakhri Kamil <fakhri.kamil1808@gmail.com>
2023-12-31 15:28:06 +0700

    Add files via upload

---
+++ stock_aplication.py
@@ -0,0 +1,18 @@
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+
+st.write(""" 
+# Simple Stock Price App 
+Shown are the stock closing price and volume of google!
+         
+""")
+
+tickerSymbol= 'MSFT'
+
+tickerData= yf.Tickers(tickerSymbol)
+
+tickerDf= tickerData.history(period='id', start='2010-1-1', end='2020-1-25')
+
+st.line_chart(tickerDf.close)
+st.line_chart(tickerDf.volume)
